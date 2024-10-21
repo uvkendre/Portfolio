b63d061c1db8e25b5cf2f2c019a0e42da3b7803c
uvkendre <yuvikendre2002@gmail.com>
2024-10-21 22:31:26 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Port-folio/', // Add this line for GitHub Pages deployment
+  base: '',
 })
